0ab037cc50f05dc87a720b68b612ebddf50e99ef
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-01-31 14:36:03 +0000

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'confyaml'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures confyaml'
+long_description 'Installs/Configures confyaml'
+version          '0.1.0'
